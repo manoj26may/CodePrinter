dc15542146ed27601a48209333f765cebb1aa5a3
Manoj M Bhat <mmbhat948@gmail.com>
2025-06-06 08:38:20 +0530

    Add files via upload

---
+++ examples/Demo/Demo.ino
@@ -0,0 +1,17 @@
+#include <CodePrinter.h>
+
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+
+  CodePrinter::Blink();
+  CodePrinter::DHT();
+  CodePrinter::OLED();
+  CodePrinter::PushButton();
+  CodePrinter::SoilMoisture();
+  CodePrinter::Ultrasonic();
+  CodePrinter::ThingSpeakUpload();
+  CodePrinter::ThingSpeakReader();
+}
+
+void loop() {}
